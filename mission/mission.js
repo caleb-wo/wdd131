d8378b1a11639bd66d9f465b161c7330c5d3bfaf
caleb-wo <camawolfe52@icloud.com>
2024-09-25 11:49:57 -0600

    Added dark mode

---
+++ mission/mission.js
@@ -0,0 +1,18 @@
+const themeSelector = document.querySelector('select[name="theme"]');
+
+function changeTheme() {
+const logo = document.querySelector('img');
+
+if (themeSelector.value === 'dark') {
+    document.body.classList.add('dark');
+    logo.src = 'byui-logo_dark.png'
+} else {
+    document.body.classList.remove('dark');
+    logo.src = 'byui-logo_blue.webpd'
+}
+
+}
+
+// add an event listener to the themeSelector element here.
+// Use the changeTheme function as the event handler function.
+themeSelector.addEventListener('change', changeTheme);
